2331595b148c6ae9899d6a585a5ae6d615f636d5
luciasalar <luciasalar@gmail.com>
2017-04-06 09:23:53 +0800

    Update sparklyr.R

--- sparklyr.R
+++ sparklyr.R
@@ -7,28 +7,6 @@ library(dplyr)
 
 sc <- spark_connect(master = "yarn-client")
 
-# weibo <- file.path("adl://weibo033adls.azuredatalakestore.net")
-# 
-# weibo1 <- spark_read_csv(sc,
-#                          path = weibo,
-#                          name = 'weibo_data_1',
-#                          header = FALSE,
-#                          delimiter = ","
-# )
-# 
-# adl://weibo033adls.azuredatalakestore.net/weibo_data_1.csv
-# 
-# 
-# https://weibo22.blob.core.windows.net/demolda-2017-04-04t11-39-24-415z/user/weibo_data_1.csv
-# 
-# myNameNode <- "adl://weibo033adls.azuredatalakestore.net"
-# myPort <- 0
-# 
-# mySparkCluster <- RxSpark(consoleOutput=TRUE, nameNode=myNameNode, port=myPort)
-# rxSetComputeContext(mySparkCluster)
-# hdfsFS <- RxHdfsFileSystem(hostName=myNameNode, port=myPort)
-# 
-# dep_weibo <- read.csv("weibo_data_1.csv",header = F, quote = "", sep = "," ,fill=TRUE,row.names=NULL)
 
 ####testing with spark
 weibo_tbl <- copy_to(sc, iris)
